08706a425568172c6f94d33ccba418245fcfbc80
Asingura Philip <asinguraphilip@gmail.com>
2024-07-02 20:52:31 +0300

    added to variable file

---
+++ variable.js
@@ -0,0 +1,19 @@
+//comments can make code readable
+//welcome to CSE javascript class
+
+/*
+Comments can make code readble,
+                     easy to reuse,
+                                and informative
+*/
+
+let message = "Hello world";  //string
+let isMale = true;            //boolean
+let unStated = undefined;     //undefined
+let nothing = null;           //null
+
+
+console.log(isMale)
+console.log(isMale)
+console.log(unStated)
+console.log(nothing)
