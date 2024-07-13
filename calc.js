556103ae9359b68b7ea90651ad39fe327c1c0e11
Asingura Philip <asinguraphilip@gmail.com>
2024-07-13 13:16:58 +0300

    added js and btn functions

---
+++ calc.js
@@ -0,0 +1,51 @@
+//let num = Number(document.getElementById("number").innerText)
+//console.log(num)
+
+let text = document.querySelector(".display").innerText
+console.log(text)
+
+
+function cls(){
+    location.reload();
+}
+let num = Number(document.getElementById("number").innerText)
+function one(){
+    text = 1 + text;
+    document.querySelector(".display").innerText = text
+}
+function two(){
+    text = 2 + text;
+    document.querySelector(".display").innerText = text
+}
+function three(){
+    text = 3 + text;
+    document.querySelector(".display").innerText = text
+}
+function four(){
+    text = 4 + text;
+    document.querySelector(".display").innerText = text
+}
+function five(){
+    text = 5 + text;
+    document.querySelector(".display").innerText = text
+}
+function six(){
+    text = 6 + text;
+    document.querySelector(".display").innerText = text
+}
+function seven(){
+    text = 7 + text;
+    document.querySelector(".display").innerText = text
+}
+function eight(){
+    text = 8 + text;
+    document.querySelector(".display").innerText = text
+}
+function nine(){
+    text = 9 + text;
+    document.querySelector(".display").innerText = text
+}
+function zero(){
+    text = 0 + text;
+    document.querySelector(".display").innerText = text
+}
