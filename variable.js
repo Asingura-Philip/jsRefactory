4535f71028b65823731e37aae3a9a4e5a8e505bd
Asingura Philip <asinguraphilip@gmail.com>
2024-07-13 11:33:53 +0300

    calc files

--- variable.js
+++ variable.js
@@ -7,13 +7,52 @@ Comments can make code readble,
                                 and informative
 */
 
-let message = "Hello world";  //string
-let isMale = true;            //boolean
-let unStated = undefined;     //undefined
-let nothing = null;           //null
+// let message = "Hello world";  //string
+// let isMale = true;            //boolean
+// let unStated = undefined;     //undefined
+// let nothing = null;           //null
 
 
-console.log(message)
-console.log(isMale)
-console.log(unStated)
-console.log(nothing)
+// console.log(message)
+// console.log(isMale)
+// console.log(unStated)
+// console.log(nothing)
+// let age = 19
+// if (age>18){
+//     console.log("you are an adult")
+// }
+// console.log( "A" - "B" + 2);
+// console.log(1 + +"2" + "2");
+// console.log("7"+"8");
+
+
+// function foo() {
+//     return bar();
+//     function bar() {
+//       return "Hello, I'm bar!";
+//     }
+//   }
+  
+//   console.log(foo());
+// var x = 10;
+
+// function foo() {
+//     console.log(x);
+//     var x = 20;
+  
+// }
+
+// foo();
+// let array1 = [1,2,3,4,5,6]
+// console.log(array1.slice(0,6))
+
+var person = {
+    name: "John",
+    age: 30,
+    greet: function() {
+      return "Hello, my name is " + this.name + " and I am " + this.age + " years old.";
+    }
+  };
+  
+  var greetFunction = person.greet;
+  console.log(greetFunction());
