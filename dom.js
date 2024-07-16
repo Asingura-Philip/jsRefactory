8c629999fce321e54ab9ab0b2e259262e08bdf31
Asingura Philip <asinguraphilip@gmail.com>
2024-07-16 23:21:41 +0300

    added files

---
+++ dom.js
@@ -0,0 +1,61 @@
+/*document.querySelector("h1").innerHTML = "hiiii"
+
+let stuff = document.getElementsByTagName("h2")
+stuff[2].innerHTML = "hiiiii"
+console.log(stuff.length)
+*/
+
+let ells = document.querySelectorAll("h2")
+ells[1].innerHTML = "hiiiiooooooooooo"
+console.log(ells)
+
+
+
+//document.getElementById("second").innerHTML = "hiiii"
+
+//document.getElementsByClassName("h1").innerHTML = "hiiii"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// text.innerText = "title"
+// document.body.appendChild(text)
+
+// let age = 6;
+// function add(number){
+//         let ans = number + 1;
+//         console.log(ans)
+
+//     }
+
+// add(age)
+
+
