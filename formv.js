8aa7e1dae998ac1d6e29e0d3fb6d11692014addb
Asingura Philip <asinguraphilip@gmail.com>
2024-07-24 08:20:01 +0300

    edits

--- formv.js
+++ formv.js
@@ -1,14 +1,18 @@
-let firstName = document.getElementById('fname').value
-let lastName = document.getElementById('lname').value
-let email = document.getElementById('email').value
-let phoneNumber = document.getElementById('phone').value
-let password = document.getElementById('password').value
+let firstName = document.getElementById("fname").value;
+let lastName = document.getElementById("lname").value;
+let email = document.getElementById("email").value;
+let phoneNumber = document.getElementById("phone").value;
+let password = document.getElementById("password").value;
 
+let submitBtn = document.getElementById("sub");
 
-let submitBtn = document.getElementById("sub")
-
-submitBtn.addEventListener(submit,function(event){
-    // event.preventDefault()
-    alert("form submitted sucessfully")
-})
-
+submitBtn.addEventListener("click", function (event) {
+    console.log(firstName.lenght)
+  event.preventDefault();
+  if (firstName.lenght < 3) {
+    console.log("please enter longer name");
+  }else{
+    console.log("valid")
+  }
+  //alert("form submitted sucessfully");
+});
