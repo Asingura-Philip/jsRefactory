4a2dca5c831377d3b547d2005111748cce2e2a5b
Asingura Philip <asinguraphilip@gmail.com>
2024-07-24 11:02:50 +0300

    to-do app

--- todo.js
+++ todo.js
@@ -1,10 +1,23 @@
 let lists = document.getElementById("lists")
 let task = document.getElementById("task")
 let btn = document.getElementById("submit")
+let del = document.getElementById("del")
+let out = document.getElementById("outer")
 
-btn.addEventListener("click",function(){
+out.style.backgroundColor = "lightblue"
+out.style.width = "400px"
+
+btn.addEventListener("click",function(event){
+    event.preventDefault()
     let item = document.createElement("li")
     lists.appendChild(item)
     item.innerText = task.value
+    task.value = ""
     
+    
+})
+
+del.addEventListener("click",function(){
+    let text = document.querySelector("li")
+    lists.removeChild(text)
 })
