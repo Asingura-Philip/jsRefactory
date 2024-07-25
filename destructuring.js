493f30acaad73d26ced87c71331ea417f59af1e5
Asingura Philip <asinguraphilip@gmail.com>
2024-07-25 15:49:53 +0300

    ds xamples

--- destructuring.js
+++ destructuring.js
@@ -1,20 +1,25 @@
 //destructuiring and spread operator
 
-
 //distructuring array
-let myArray = [12,24,36]
+let myArray = [12, 24, 36];
 // myArray.forEach(element => {
 //     console.log(element)
 // });
 
-
-let[num1,num2,num3] = myArray
+let [num1, num2, num3] = myArray;
 // console.log(num1)
 
 //example 2
 
-let names = ["john","jack","julia","julien"]
-let [boy1,boy2,girl1] = names
-console.log(girl1)
-console.log(names)
+let names = ["john", "jack", "julia", "julien"];
+let [boy1, boy2, girl1] = names;
+// console.log(girl1)
+// console.log(names)
 
+let fullStack = [
+  ["html", "css", "bootstrap", "js"],
+  ["node", "express", "mongoDB"],
+];
+let [frontEnd, backEnd] = fullStack;
+
+console.log(frontEnd);
