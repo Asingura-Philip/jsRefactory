6e0fc7e4c45932763c24ebab8fa3ee346d89a9e6
Asingura Philip <asinguraphilip@gmail.com>
2024-07-25 12:01:45 +0300

    constructors js using classes

--- opp.js
+++ opp.js
@@ -15,6 +15,35 @@ let person3 = new Person("tom",24)
 let person4 = new Person("jim",25)
 let person5 = new Person("sam",15)
 
-console.log(person3.name)
-console.log(person4.name)
-console.log(person5.name)
+// console.log(person3.name)
+// console.log(person4.name)
+// console.log(person5.name)
+
+//ES6 way of writing constructors
+
+class People{
+    constructor(name,age){
+        this.name = name
+        this.age = age
+    }
+}
+let people1 = new People("toby",29)
+let people2 = new People("tony",22)
+let people3 = new People("rony",45)
+let people4 = new People("jony",34)
+
+// console.log(people1,people2,people3,people4)
+
+
+class Rectangle{
+    constructor(length,width){
+        this.length = length
+        this.width = width
+    }
+}
+
+let rect1 = new Rectangle(20,12)
+let rect2 = new Rectangle(13,5)
+let rect3 = new Rectangle(10,20)
+
+console.log(rect1,rect2,rect3)
