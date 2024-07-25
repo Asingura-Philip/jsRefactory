8bc4d5c42a8f1122e7a26461aa437840719f0459
Asingura Philip <asinguraphilip@gmail.com>
2024-07-25 15:41:20 +0300

    destructuring arrays

---
+++ destructuring.js
@@ -0,0 +1,20 @@
+//destructuiring and spread operator
+
+
+//distructuring array
+let myArray = [12,24,36]
+// myArray.forEach(element => {
+//     console.log(element)
+// });
+
+
+let[num1,num2,num3] = myArray
+// console.log(num1)
+
+//example 2
+
+let names = ["john","jack","julia","julien"]
+let [boy1,boy2,girl1] = names
+console.log(girl1)
+console.log(names)
+
