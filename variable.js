422ce0a8f7101bb5d1ac625b241e1809179905d9
Asingura Philip <asinguraphilip@gmail.com>
2024-07-28 22:02:13 +0300

    function that checks number of syllables

--- variable.js
+++ variable.js
@@ -12,7 +12,6 @@ Comments can make code readble,
 // let unStated = undefined;     //undefined
 // let nothing = null;           //null
 
-
 // console.log(message)
 // console.log(isMale)
 // console.log(unStated)
@@ -25,34 +24,75 @@ Comments can make code readble,
 // console.log(1 + +"2" + "2");
 // console.log("7"+"8");
 
-
 // function foo() {
 //     return bar();
 //     function bar() {
 //       return "Hello, I'm bar!";
 //     }
 //   }
-  
+
 //   console.log(foo());
 // var x = 10;
 
 // function foo() {
 //     console.log(x);
 //     var x = 20;
-  
+
 // }
 
 // foo();
 // let array1 = [1,2,3,4,5,6]
 // console.log(array1.slice(0,6))
 
-var person = {
-    name: "John",
-    age: 30,
-    greet: function() {
-      return "Hello, my name is " + this.name + " and I am " + this.age + " years old.";
+// var person = {
+//     name: "John",
+//     age: 30,
+//     greet: function() {
+//       return "Hello, my name is " + this.name + " and I am " + this.age + " years old.";
+//     }
+//   };
+
+//   var greetFunction = person.greet;
+//   console.log(greetFunction());
+
+//create a function that counts the number of syllables a word has
+//each syllable is separated eith a dash
+
+// function numberSyllables(word) {
+//   let myArray = word.split("");
+//   let sep = 0;
+//   for (let i = 0; i < myArray.lenght; i++) {
+//     console.log(i);
+//     if (myArray[i] === "-") {
+//       sep = sep + 1;
+//     }
+//   }
+//   console.log(sep);
+// }
+
+// numberSyllables("ba-na-na");
+
+// let word = "a-bc-dc-ju";
+function newSyll(word) {
+  let myArray = word.split("");
+  console.log(myArray);
+  let sep = 1;
+  for (let i = 0; i < myArray.length; i++) {
+    if (myArray[i] === "-") {
+      sep = sep + 1;
     }
-  };
-  
-  var greetFunction = person.greet;
-  console.log(greetFunction());
+  }
+  console.log(` ${word} contains ${sep} syllables`);
+}
+newSyll("ma-ca-ro-ni")
+// // let name = "saitama";
+// // console.log(name.split(''));
+
+// let myArray = ["b", "a", "-", "n", "a", "-", "n", "a"];
+// let sep = 0;
+// for (let i = 0; i < myArray.lenght; i++) {
+//   if (myArray[i] === "a") {
+//     sep = sep + 1;
+//   }
+// }
+// console.log(sep);
