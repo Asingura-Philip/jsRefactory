2e4299353da619f0faa20a57c1cbfcc5af40b970
Asingura Philip <asinguraphilip@gmail.com>
2024-10-11 23:17:24 +0300

    closures ex 2

--- more.js
+++ more.js
@@ -7,5 +7,25 @@ function outer(){
     function inner(){
         console.log(message)
     }
-    
+    inner()
 }
+
+// outer()
+
+// #2
+function createCounter(){
+
+
+    let count = 0;
+    function increment(){
+        
+        count++;
+        console.log(`count increased to ${count}`)
+    }
+    return {increment}
+}
+
+const counter = createCounter()
+counter.increment()
+counter.increment()
+counter.increment()
